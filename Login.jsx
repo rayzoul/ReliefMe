400b752a369a77c1ad4df0c5e0b4ade0780c1b6c
rayzoul <82937727+rayzoul@users.noreply.github.com>
2023-06-26 15:16:11 +0700

    Update Login.jsx

--- Login.jsx
+++ Login.jsx
@@ -3,6 +3,8 @@ import { signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, Facebo
 import { auth, db } from '../firebase';
 import { collection, addDoc } from 'firebase/firestore'; // เพิ่ม import นี้
 
+import { BsGoogle, BsFacebook, BsX, BsEye, BsEyeSlash } from "react-icons/bs";
+
 function Login({ onClose }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -23,69 +25,57 @@ function Login({ onClose }) {
   const createUserData = async (user) => {
     try {
       const randomID = Math.floor(Math.random() * 1000000); // สุ่มตัวเลขระหว่าง 0-999999
-  
+
       const userData = {
         admin_id: `001${randomID}`,
         user_email: user.email,
         user_fullname: '...',
         user_id: `001${randomID}`,
       };
-  
+
       const userRef = await addDoc(collection(db, 'user'), userData);
       console.log('เพิ่มข้อมูลผู้ใช้งานเรียบร้อยแล้ว', userRef.id);
-  
+
       const avatarData = {
         avatar_id: `002${randomID}`,
         avatar_url: '...',
         user_id: `001${randomID}`
         // เพิ่มข้อมูลอื่น ๆ เกี่ยวกับ avatar ตามต้องการ
       };
-  
+
       const avatarRef = await addDoc(collection(db, 'user', userRef.id, 'avatar'), avatarData);
       console.log('เพิ่มข้อมูล avatar เรียบร้อยแล้ว', avatarRef.id);
-      
+
       const frameData = {
         frame_id: `003${randomID}`,
         frame: '...',
         user_id: `001${randomID}`
         // เพิ่มข้อมูลอื่น ๆ เกี่ยวกับ frame ตามต้องการ
       };
-  
+
       const frameRef = await addDoc(collection(db, 'user', userRef.id, 'frame'), frameData);
       console.log('เพิ่มข้อมูล frame เรียบร้อยแล้ว', frameRef.id);
-  
+
       const walletData = {
         wallet_id: `004${randomID}`,
         wallet_sum: 0,
         check_day_id: 0
         // เพิ่มข้อมูลอื่น ๆ เกี่ยวกับ wallet ตามต้องการ
       };
-  
+
       const walletRef = await addDoc(collection(db, 'user', userRef.id, 'wallet'), walletData);
       console.log('เพิ่มข้อมูล wallet เรียบร้อยแล้ว', walletRef.id);
 
-      const checkdayData = {
-        check_day_id: `005${randomID}`,
-        check_day_name: '...',
-        coin: 0,
-        diary_id: `006${randomID}`
-      };
-      
-      const checkdayRef = await addDoc(collection(db, 'user', userRef.id, 'wallet', walletRef.id, 'check_day'), checkdayData);
-      console.log('เพิ่มข้อมูล checkday เรียบร้อยแล้ว', checkdayRef.id);
-      
-      const coinData = {
-        coin:0
-      };
-      
-      const coinRef = await addDoc(collection(db, 'user', userRef.id, 'wallet', walletRef.id, 'coin'), coinData);
-      console.log('เพิ่มข้อมูล checkday เรียบร้อยแล้ว', coinRef.id);
-  
+
     } catch (error) {
       console.error('Error creating user data:', error);
     }
   };
-  
+
+
+
+
+
 
 
   const handleGoogleLogin = async () => {
@@ -114,54 +104,58 @@ function Login({ onClose }) {
 
 
   return (
-    <div className="bg-white w-96 rounded-md p-6">
-      <h2 className="text-xl font-medium mb-4">เข้าสู่ระบบ</h2>
+    <div className="bg-white w-96 rounded-[16px] p-8 relative">
+      <button className='absolute top-3 right-3 text-[32px]' onClick={onClose}><BsX /></button>
+      <h2 className="text-[36px] text-center text-[#151515] font-medium mb-2 fontMitr ">เข้าสู่ระบบ</h2>
+      <p className='text-center font-light text-[#808080] mb-4 fontMitr'>เข้าสู่ระบบของคุณด้วยอีเมล</p>
       <form className="space-y-4">
         <div>
-          <label htmlFor="email">อีเมล:</label>
+          <label htmlFor="email" className='fontMitr'>อีเมล:</label>
           <input
             type="email"
             id="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            className="w-full border border-gray-300 rounded-md px-3 py-2"
+            className="fontMitr w-full border border-gray-300 rounded-md px-3 py-2 bg-[#fff] text-[#151515]"
           />
         </div>
         <div>
-          <label htmlFor="password">รหัสผ่าน:</label>
+          <label htmlFor="password" className='fontMitr'>รหัสผ่าน:</label>
           <input
             type="password"
             id="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className="w-full border border-gray-300 rounded-md px-3 py-2"
+            className=" fontMitr w-full border border-gray-300 rounded-md px-3 py-2 bg-[#fff] text-[#151515]"
           />
+
         </div>
-        <div className="flex justify-end">
+        <div className="flex  justify-center">
           <button
             type="button"
-            className="btn btn-primary"
+            className="fontMitr font-medium w-full btn bg-[#EC7966] text-[#fff] hover:bg-[#F2A194]"
             onClick={handleEmailLogin}
           >
             เข้าสู่ระบบ
           </button>
         </div>
       </form>
-      <p className="text-center text-sm mt-4">
-        หรือเข้าสู่ระบบด้วย:
+      <div className='mt-4 h-[0.5px] bg-[#E0E0E0]'></div>
+      <p className="text-[#808080] fontMitr text-center text-sm mt-4">
+        หรือเข้าสู่ระบบด้วย
       </p>
       <div className="flex justify-center space-x-4 mt-4">
         <button
-          className="btn btn-primary"
+          className="btn fontMitr font-medium bg-[#F2F2F3] text-[#151515] hover:bg-[#B9B9B9] rounded-full"
           onClick={handleGoogleLogin}
         >
-          Google
+          <BsGoogle /> Google
         </button>
         <button
-          className="btn btn-primary"
+          className="btn fontMitr font-medium bg-[#F2F2F3] text-[#151515] hover:bg-[#B9B9B9] rounded-full"
           onClick={handleFacebookLogin}
         >
-          Facebook
+          <BsFacebook /> Facebook
         </button>
       </div>
     </div>
